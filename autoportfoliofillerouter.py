23d40f96ed745169b4b6483f1194dbcfaacd26bf
Dialarmsaw <83833689+Dialarmsaw@users.noreply.github.com>
2023-12-07 09:42:11 -0500

    Create autoportfoliofillerouter.py

---
+++ autoportfoliofillerouter.py
@@ -0,0 +1,15 @@
+'''
+For this project, I made a ____ about ___. This ____ talks about how _________________. 
+This represents ________ because _____.
+'''
+
+projectType = input("What type of project (ex. essay or poster) >>> ")
+projectSubg = input("What was the project subject (ex. climate change or gender equality)? >>> ")
+projectAbout = input("What was the project about (ex. this guy was really cool)? >>> ")
+skill = input("What skill does this project represent (autonomy, complexity, or awareness) >>> ")
+whySkill = input("Why does it represent that? >>> ")
+
+print("\n\n\n" + classes + "\n" + projectName + "\n" + year + " " + trimester)
+
+print("For this project, I made a " + projectType + " about " + projectSubg + ". This " + projectType + 
+" talks about how " + projectAbout + ". This represents " + skill + " because " + whySkill + ".")
